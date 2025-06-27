66d1a250427b3d5765871a1b5b33ea2007a9a405
m7mdyoussef <mohamedyoussef19992@gmail.com>
2025-06-27 08:39:34 +0300

    create project arch and folders

---
+++ FindMyCountry/FindMyCountry/App/FindMyCountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyCountryApp.swift
+//  FindMyCountry
+//
+//  Created by Mohamed Youssef Al-Azizy on 27/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct FindMyCountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
